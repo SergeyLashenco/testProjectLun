2cc10acc87a5ca4989d99a017022ede0642d6872
SergeyLashenko <sergeylashenco@gmail.com>
2020-11-25 15:45:19 +0200

    add Tag and upDate build.gradle

--- src/test/java/org/example/tests/TestOpenSiteLun.java
+++ src/test/java/org/example/tests/TestOpenSiteLun.java
@@ -1,12 +1,15 @@
 package org.example.tests;
 
 import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$$;
 
 public class TestOpenSiteLun extends TestBase {
 
+
+    @Tag("web")
     @Test
     public void testOpenSiteLun () {
         openSiteLun();
--- src/test/java/org/example/tests/TestSearchBuildingsContinentRay.java
+++ src/test/java/org/example/tests/TestSearchBuildingsContinentRay.java
@@ -1,9 +1,11 @@
 package org.example.tests;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class TestSearchBuildingsContinentRay extends TestBase {
 
+    @Tag("web")
     @Test
     public void testSearchBuildingsContinentRay(){
         openSiteLun();
